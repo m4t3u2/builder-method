c9b71587ff76b2221d5f2abd2aebf725428703fb
Mateus Simon <ms.mateus@hotmail.com>
2022-08-19 15:20:10 -0300

    Envio para o GitHub

---
+++ src/main/java/model/builder/PedidoVendaBuilderValido.java
@@ -0,0 +1,17 @@
+package model.builder;
+
+import model.PedidoVenda;
+
+public class PedidoVendaBuilderValido {
+
+	private PedidoVenda instancia;
+
+	public PedidoVendaBuilderValido(PedidoVenda instancia) {
+		this.instancia = instancia;
+	}
+
+	public PedidoVenda construir() {
+		return this.instancia;
+	}
+
+}
---
+++ src/test/java/model/PedidoVendaTest.java
@@ -0,0 +1,58 @@
+package model;
+
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import model.builder.PedidoVendaBuilder;
+
+public class PedidoVendaTest {
+	
+	// Exemplo sem Builder.
+	@Test
+	public void deveCalcularValorTotalPedidoParaClienteVip() {
+		PedidoVenda pedidoVenda = new PedidoVenda();
+		
+		Cliente cliente = new Cliente();
+		cliente.setNome("João");
+		cliente.setVip(true);
+		pedidoVenda.setCliente(cliente);
+		
+		ItemPedido item1 = new ItemPedido();
+		item1.setNome("Calculadora");
+		item1.setValorUnitario(new BigDecimal("200"));
+		item1.setQuantidade(2);
+		
+		ItemPedido item2 = new ItemPedido();
+		item2.setNome("Mochila");
+		item2.setValorUnitario(new BigDecimal("200"));
+		item2.setQuantidade(1);
+		
+		List<ItemPedido> itensPedido = Arrays.asList(item1, item2);
+		pedidoVenda.setItensPedido(itensPedido);
+		
+		BigDecimal valorTotal = pedidoVenda.getValorTotal();
+		
+		assertEquals(new BigDecimal("576").doubleValue(), valorTotal.doubleValue(), 0.0001);
+	}
+	
+	// Exemplo com Builder.
+	@Test
+	public void deveCalcularValorTotalPedidoParaClienteVipComBuilder() {
+		PedidoVenda pedidoVenda = new PedidoVendaBuilder()
+										.comClienteVip("João Silva")
+										.comItem("Calculadora", 2, "200")
+										.comItem("Mochila", 1, "200")
+										.comNumero("01234")
+										.construir();
+		
+		BigDecimal valorTotal = pedidoVenda.getValorTotal();
+		
+		assertEquals(new BigDecimal("576").doubleValue(), valorTotal.doubleValue(), 0.0001);
+	}
+
+}
